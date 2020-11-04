3f229a3ea2518b3f683cdcd7f09cf38f44de3471
Rudi Setiawan <rudestewing@gmail.com>
2020-11-04 12:19:13 +0700

    -

---
+++ Stack.js
@@ -0,0 +1,33 @@
+class StackList {
+    constructor() {
+        this.items = [];
+    }
+
+    peek() {
+        let item = this.items[this.items.length -1];
+        console.log(item);
+        return item;
+    }
+
+    add(item) {
+        this.items.push(item);
+    }
+
+    pop() {
+       this.items.pop(); 
+    }
+
+    print() {
+        console.log(this.items);
+    }
+}
+
+let sl = new StackList();
+
+sl.add('kucing');
+sl.add('anjing');
+
+sl.peek();
+sl.pop();
+
+sl.print();
